8863154fb5d6e756257215b2b28e6447a0fbf8f9
MaxMali <max.malinowski@web.de>
2020-05-17 02:12:54 +0200

    just the first files
    Game.py, Player.py , Bot.py

---
+++ Game.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+"""
+Project Bitcoin Course Tip Game
+
+This project was created as a subtask of the Data Science module of the Media Informatics Master course.
+
+Written by Max M., 2020
+"""
+
+
+class Game:
+
+    def __init__(self):
+        self.name = "Generic Event"
+        self.time_submission = 10
+        self.time_round = 5
+        self.number_of_player = 0
---
+++ Player.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+"""
+Project Bitcoin Course Tip Game
+
+This project was created as a subtask of the Data Science module of the Media Informatics Master course.
+
+Written by Max M., 2020
+"""
